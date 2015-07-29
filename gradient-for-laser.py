4d9441bbbd417d8018d44b6e18b55e3c8360de5f
Eric Tang <erictang@google.com>
2015-07-29 01:12:42 +0100

    can create a new layer and modify pixels on it

--- gradient-for-laser.py
+++ gradient-for-laser.py
@@ -3,8 +3,29 @@
 from gimpfu import *
 
 def gradient_for_laser(img, drawable):
-    pdb.gimp_message("hello")
+    pdb.gimp_message("begin")
     
+    # Set up an undo group, so the operation will be undone in one step.
+    pdb.gimp_undo_push_group_start(img)
+    
+    destDrawable = gimp.Layer(img, "new layer", drawable.width, drawable.height, drawable.type, drawable.opacity, drawable.mode);
+    img.add_layer(destDrawable, 0)
+    dest_rgn = destDrawable.get_pixel_rgn(0, 0, drawable.width, drawable.height, True, True)
+
+    gimp.progress_init("running")
+    for y in range(0, destDrawable.height):
+        for x in range(0, 16):
+            dest_rgn[x, y] = chr(255) + '\0\0'# + chr(255)
+        gimp.progress_update(float(y) / destDrawable.height)
+
+    destDrawable.flush()
+    destDrawable.merge_shadow(True)
+    destDrawable.update(0, 0, destDrawable.width, destDrawable.height)
+
+    # Close the undo group.
+    pdb.gimp_undo_push_group_end(img)
+
+    pdb.gimp_message("done")
     return
 
 register(
